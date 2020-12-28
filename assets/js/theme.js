70a4854b7dbb83937160476b04f36a10cd469151
Rohit <rohitjain18005@gmail.com>
2020-12-28 20:04:15 +0530

    added custom highlight CSS

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,12 @@
 const themeMap = {
   'default': {
     'background-color': 'white',
-    'text-color': '#222'
+    'text-color': '#222',
+    'highlight-color': '#eee'
   },
   'dark': {
     'background-color': '#222',
-    'text-color': 'white'
+    'text-color': 'white',
+    'highlight-color': '#2e2e2e'
   }
 }
